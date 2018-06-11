3c419c8b7291b515c751c3179cf1125d3d56ecc7
Yahshemi Walters <Yahshemi@gmail.com>
2018-06-11 15:14:38 -0700

    adds initial project files

---
+++ src/js/app.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  // your Javascript goes here
+  //A constructor method to initialize the state of the app
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 0,
+      rate: 0,
+      term: 15,
+      result: 0
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  handleChange(e) {
+
+    var name = e.target.name;
+    var value = e.target.value;
+    this.setState({ [name]: value })
+  }
+
+  handleClick() {
+    console.log('handleClick()')
+
+    var principal = this.state.balance;
+    var n = this.state.term * 12;
+    var r = this.state.rate / 1200;
+    var m = principal * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+    this.setState({ result: m })
+
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        {/* your JSX goes here */}
+        <h1>Mortgage Calculator</h1>
+        <input name='balance' type='number' onChange={this.handleChange} />
+        <input name='rate' type='number' step='0.01' onChange={this.handleChange} />
+        <select name='term' onChange={this.handleChange}>
+          <option value='15'>15</option>
+          <option value='30'>30</option>
+        </select>
+        <button name='submit' onClick={this.handleClick}>Calculate</button>
+        <div id='output'>Your monthly payment is {new Intl.NumberFormat("dollars", { style: 'currency', currency: 'USD' }).format(this.state.result)}</div>
+        
+
+      </div>
+    )
+
+  }
+}
+
+
